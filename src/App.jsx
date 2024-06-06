5dc338873e38c6de6416084206231b2436c1984a
iamsoumikhazra <www.soumikhazra@gmail.com>
2024-06-06 21:45:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { countAtom } from './store/atoms/count';
+
+function App() {
+  const [count, setCount] = useRecoilState(countAtom);
+  
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </>
+  );
+}
+
+export default App;
